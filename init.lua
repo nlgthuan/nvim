22d8c3f7f38b4e9993a72ebe547dc65d64674bd9
Thuan Ngo <thuan.ngolegia@gmail.com>
2025-12-09 17:41:27 +0700

    formatter and telescope

--- init.lua
+++ init.lua
@@ -30,19 +30,6 @@ vim.opt.timeoutlen = 300
 -- [[ Diagnostic ]]
 vim.diagnostic.config({ virtual_text = true })
 
--- [[ Key map ]]
-
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" }) 
-
-vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]], {desc = "Copy to clipboard" })
-
--- Center when jumping
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
-vim.keymap.set('n', 'n', 'nzzzv')
-vim.keymap.set('n', 'N', 'Nzzzv')
-
-
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking',
   group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
@@ -54,4 +41,27 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 require("config.lazy")
 
-vim.lsp.enable({"lua_ls", "ts_ls"})
+-- [[ LSP ]]
+vim.lsp.enable({ "lua_ls", "ts_ls" })
+
+
+-- [[ Key map ]]
+
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]], { desc = "Copy to clipboard" })
+
+-- Center when jumping
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+
+
+-- Format
+vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format() end)
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = 'Telescope help tags' })
