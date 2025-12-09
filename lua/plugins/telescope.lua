376e7122e082ebfbecd08108c41c60e989281f60
Thuan Ngo <thuan.ngolegia@gmail.com>
2025-12-10 00:37:30 +0700

    update telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,6 +3,13 @@ return {
   tag = 'v0.2.0',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
+    require('telescope').setup {
+      defaults = {
+        mappings = {
+          i = { ['<c-q>'] = 'smart_send_to_qflist' },
+        },
+      },
+    }
     require("config.telescope.multigrep").setup()
   end
 }
